9b99cf976437c8cb985935fe53b9ff37c27a1f5d
ruki-qq <ruki@duck.com>
2023-09-15 21:01:25 +0300

    Fix datetime form and move user forms & views to users app

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-from .views import UsersCreateView
+from users.views import UsersCreateView
 
 handler404 = 'pages.views.page_not_found'
 handler500 = 'pages.views.internal_server_error'
